2f2f6ca0eede99af470ea4fee95dc4dfb3f65bc9
SangLM <Sanglm2207@gmail.com>
2025-10-02 13:46:56 +0700

    init

---
+++ src/main/java/com/kaidev99/ecommerce/EcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.kaidev99.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+}
